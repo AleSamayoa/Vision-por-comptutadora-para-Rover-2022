e522b07acd0da58102950c2f8e0afa9b34121fef
AleSamayoa <69053381+AleSamayoa@users.noreply.github.com>
2022-09-10 12:59:34 -0600

    Update ConectarWifi.sh

--- Documentacion Imagen/ConectarWifi.sh
+++ Documentacion Imagen/ConectarWifi.sh
@@ -1,8 +1,49 @@
 #/bin/bash
-echo "Ingrese el nombre de la red a la que quiere ingresar entre comillas:"
-read name
-echo "Ingrese la contraseña entre comillas. Si la red no tiene contraseña, ingrese la letra n:"
-read contra
+PS3="Seleccione una de las redes predeterminadas o seleccione la opción de crear una nueva red: "
+
+select red in UVG Robotat Otra;
+do 
+  case $red in
+    UVG)
+printf '%s\n' "
+network:
+    ethernets:
+        eth0:
+            optional: true
+            dhcp4: true
+    version: 2
+       
+    wifis:
+       wlan0:
+           optional: true
+           access-points:
+             \"UVG\": {}
+           dhcp4: true  
+"> /etc/netplan/50-cloud-init.yaml
+      ;;
+    Robotat) 
+printf '%s\n' "
+network:
+    ethernets:
+        eth0:
+            optional: true
+            dhcp4: true
+    version: 2
+            
+    wifis:
+       wlan0:
+           optional: true
+           access-points:
+             \"Robotat\": 
+                 password: \"iemtbmcit116\" 
+           dhcp4: true  
+        "> /etc/netplan/50-cloud-init.yaml
+      ;;
+    Otra)
+      echo "Ingrese el nombre de la red a la que quiere ingresar:"
+      read name
+      echo "Ingrese la contraseña de la red. Si la red no tiene contraseña, ingrese la letra n:"
+      read contra
 if [[ $contra == "N" || $contra == "n" ]]; then
 printf '%s\n' "
 network:
@@ -16,7 +57,7 @@ network:
        wlan0:
            optional: true
            access-points:
-             $name: {}
+             \"$name\": {}
            dhcp4: true  
 "> /etc/netplan/50-cloud-init.yaml
 
@@ -33,11 +74,17 @@ network:
        wlan0:
            optional: true
            access-points:
-             $name:
-                 password: $contra
+             \"$name\":
+                 password: \"$contra\"
            dhcp4: true
               
 "> /etc/netplan/50-cloud-init.yaml
 
-fi
+fi     
+      ;;
+      *)
+      echo "Opción inválida, por favor seleccione una de las opciones mostradas";;
+  esac
+  break
+done
 netplan apply
